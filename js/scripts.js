779034a9dc6b14eb1a927be8eb33acdfb8060022
Omar Solano and Sam Gespass <sam@>
2017-11-30 15:57:54 -0800

    add images, background images, css to hide images and js to display correct image for character

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,47 @@ $(document).ready(function() {
     var rich = $("input:radio[name=5]:checked").val();
     var artist = $("input:radio[name=6]:checked").val();
     var sass = $("input:radio[name=7]:checked").val();
+    $(".nihilist").hide();
+    $(".maude").hide();
+    $(".bunny").hide();
+    $(".china").hide();
+    $(".lebowski").hide();
+    $(".cowboy").hide();
+    $(".dude").hide();
+    $(".walter").hide();
+    $(".jesus").hide();
+    $(".donny").hide();
 
     if (nihilist === "yes") {
       alert("You are a nihilist");
+      $(".nihilist").show();
     } else if (johnson === "no" && artist === "yes") {
-      alert("You are Maude")
+      alert("You are Maude");
+      $(".maude").show();
     } else if (johnson === "no" && artist === "no") {
-      alert("You are Bunny")
+      alert("You are Bunny");
+      $(".bunny").show();
     } else if (rugs === "no" && johnson === "yes") {
-      alert("You're the Chinaman who peed on my rug!")
+      alert("You're the Chinaman who peed on my rug!");
+      $(".china").show();
     } else if (rich === "yes" && artist === "no" && children === "yes") {
-      alert("You are Mr. Lebowski")
+      alert("You are Mr. Lebowski");
+      $(".lebowski").show();
     } else if (sass === "yes") {
-      alert("You are the mysterious cowboy narrator played by Sam Elliott")
+      alert("You are the mysterious cowboy narrator played by Sam Elliott");
+      $(".cowboy").show();
     } else if (rugs === "yes" && rich === "no" && children === "no") {
-      alert("You're Dude")
+      alert("You're Dude");
+      $(".dude").show();
     } else if (rugs === "yes" && artist === "no" && children === "no") {
-      alert("You are Walter")
+      alert("You are Walter");
+      $(".walter").show();
     } else if (children === "yes" && artist === "yes") {
       alert("You are The Jesus")
+      $(".jesus").show();
     } else {
       alert("STFU Donny!")
+      $(".donny").show();
     }
   });
 });
